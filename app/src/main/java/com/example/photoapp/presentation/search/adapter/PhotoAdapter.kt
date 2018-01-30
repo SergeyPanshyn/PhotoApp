3ae45d4295afca441545ee1f819d88f63aaaa14c
SergeyPanshyn <panshyn.s@gmail.com>
2018-01-30 15:22:34 +0200

    removed Picasso library, added Glide library

--- app/src/main/java/com/example/photoapp/presentation/search/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/photoapp/presentation/search/adapter/PhotoAdapter.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.photoapp.R
 import com.example.photoapp.data.api.models.PhotoItem
-import com.squareup.picasso.Picasso
 
 /**
  * Created by Sergey Panshyn on 03.11.2017.
@@ -17,8 +17,9 @@ class PhotoAdapter(val context: Context, var data: List<PhotoItem>): RecyclerVie
 
     override fun onBindViewHolder(holder: PhotoViewHolder?, position: Int) {
         val currentData = data[position]
-        Picasso.with(context).load(currentData.imageUrl).into(holder?.mainIv)
         holder?.photoName?.text = currentData.title
+        Glide.with(context).load(currentData.imageUrl).into(holder?.mainIv!!)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): PhotoViewHolder {
